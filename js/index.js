2acf09e2fc060d016d75682970c14cd91da19a04
Matt Gottsacker <mgottsacker34@gmail.com>
2019-08-19 10:38:42 -0500

    Security updates, fix most recent assignment link

--- js/index.js
+++ js/index.js
@@ -8,10 +8,10 @@ function main() {
     });
 
     $("#most-recent-assignment").load("html/assignments.html table tr:last td:eq(2) a", function() {
-        let assignment_link = $("#most-recent-assignment").attr("href");
-        console.log(assignment_link);
+        let assignment_link = $("#most-recent-assignment a").attr("href");
+        // console.log(assignment_link);
         assignment_link = assignment_link.replace(/assignments\//, "html/assignments/");
-        console.log(assignment_link);
+        // console.log(assignment_link);
         $("#most-recent-assignment a").attr("href", assignment_link);
     });
 }
