cd2bfe9a3c4b35be9435e4b2cf1d0057e4dea781
Matt Gottsacker <mgottsacker34@gmail.com>
2019-08-20 17:14:24 -0500

    wide-ranging frontend updates

--- js/index.js
+++ js/index.js
@@ -7,11 +7,16 @@ function main() {
         $("#most-recent-lecture a").attr("href", lecture_link);
     });
 
-    $("#most-recent-assignment").load("html/assignments.html table tr:last td:eq(2) a", function() {
+    // $("#most-recent-assignment").load("html/assignments.html table tr:last td:eq(2) a", function() {
+    //     let assignment_link = $("#most-recent-assignment a").attr("href");
+    //     // console.log(assignment_link);
+    //     assignment_link = assignment_link.replace(/assignments\//, "html/assignments/");
+    //     // console.log(assignment_link);
+    //     $("#most-recent-assignment a").attr("href", assignment_link);
+    // });
+    $("#most-recent-assignment").load("html/assignments.html #last-assignment", function() {
         let assignment_link = $("#most-recent-assignment a").attr("href");
-        // console.log(assignment_link);
         assignment_link = assignment_link.replace(/assignments\//, "html/assignments/");
-        // console.log(assignment_link);
         $("#most-recent-assignment a").attr("href", assignment_link);
     });
 }
